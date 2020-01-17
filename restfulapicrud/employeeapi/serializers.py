2a0c80ca1d52c126dca5b9abded6869c292b0c63
rortiz <rortiz@uca.edu.sv>
2020-01-17 17:12:56 -0600

    First commit

---
+++ restfulapicrud/employeeapi/serializers.py
@@ -0,0 +1,8 @@
+# api <-> mobile_app / web_app/ etc. -> json/xml
+from rest_framework  import serializers
+from .models import Employee
+
+class EmployeeSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Employee
+		fields = '__all__' #('id','fullname')
