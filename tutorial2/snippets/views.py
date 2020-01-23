2b66c17c4e9fbe1fa8b4607db820bbd9cf7740e3
rortiz <rortiz@uca.edu.sv>
2020-01-23 11:44:13 -0600

    Relationships & Hyperlinked APIs

--- tutorial2/snippets/views.py
+++ tutorial2/snippets/views.py
@@ -3,7 +3,10 @@ from django.contrib.auth.models import User
 from snippets.models import Snippet
 from snippets.permissions import IsOwnerOrReadOnly
 from snippets.serializers import SnippetSerializer, UserSerializer
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, renderers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.reverse import reverse
 
 class SnippetList(generics.ListCreateAPIView):
 	queryset = Snippet.objects.all()
@@ -28,7 +31,20 @@ class UserDetail(generics.RetrieveAPIView):
 	serializer_class = UserSerializer
 
 
+@api_view
+def api_root(request, format=None):
+	return Response({
+			'users': reverse('user-list', request=request, format=format),
+			'snippets': reverse('snippet-list', request=request, format=format)
+		})
 
+class SnippetHighlight(generics.GenericAPIView):
+	queryset = Snippet.objects.all()
+	renderer_classes = [renderers.StaticHTMLRenderer]
+
+	def get(self, request, *args, **kwargs):
+		snippet = self.get_object()
+		return Response(snippet.highlighted)
 
 ##MIXINS
 #from snippets.models import Snippet
--- tutorial2/tutorial2/settings.py
+++ tutorial2/tutorial2/settings.py
@@ -71,6 +71,10 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tutorial2.wsgi.application'
 
+REST_FRAMEWORK ={
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 1
+}
 
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
